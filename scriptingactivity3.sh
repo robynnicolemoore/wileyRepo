62f48e96e5c3f6ae6c52cc63bc4bc1c60722fb2a
Robyn <robynnicolemoore@gmail.com>
2022-05-25 19:35:02 +0000

    Fixing activity 3

--- scriptingactivity3.sh
+++ scriptingactivity3.sh
@@ -7,8 +7,8 @@ DATE=`date +%m.%d.%Y`
 echo $RAN_NUM $LUCKY_NUM > /home/ec2-user/wileyRepo/lottery.txt+$DATE
 echo "Enter your lottery numbers"
 read input
-echo "The numbers you put were $input"
+echo $input > /home/ec2-user/wileyRepo/lotteryguesser.txt+$DATE
 
 
-diff [-C]  $input /home/ec2-user/wileyRepo/lottery.txt+$DATE
+diff /home/ec2-user/wileyRepo/lotteryguesser.txt+$DATE  /home/ec2-user/wileyRepo/lottery.txt+$DATE
 echo "$diff"
