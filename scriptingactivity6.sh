3bf9bb3f525166bc4984ea354339db5b6463e581
Robyn <robynnicolemoore@gmail.com>
2022-05-29 02:04:11 +0000

    Fixing activity6 - Done

--- scriptingactivity6.sh
+++ scriptingactivity6.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-echo "Put in one number in the form of a numeral"
-read input
-VAL1=$input
+echo "Put in a number in the form of a numeral"
+read VAL1
+
+while [[ ! $VAL1 =~ ^[0-9]+$ ]]
+do
+	echo "Please enter only numbers in the form of numerals"
+	exit 1
+done
+
 echo "Put in another number in the form of a numeral"
-read input2
-VAL2=$input2
+read VAL2
+
+while [[ ! $VAL2 =~ ^[0-9]+$ ]]
+do
+	echo "Please enter only numbers in the form of numerals"
+	exit 1
+done
 
 if (( $VAL1 > $VAL2 ))
 then
